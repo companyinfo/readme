196fa23f0c28295472d2e64244f59cad3088ba74
wolter <mike.wolter@company.info>
2026-09-08 10:26:59 +0200

    expanded `APIDefinition` struct with additional fields and nested types

--- api_definition_dto.go
+++ api_definition_dto.go
@@ -1,12 +1,34 @@
 package readme
 
+import "time"
+
 // APIDefinition models the response from the ReadMe API v2 for API definitions.
 //
 // See: https://docs.readme.com/main/reference/getapis
 type APIDefinition struct {
-	ID      string `json:"id"`
-	Version string `json:"version"` // branch/version slug
-	Title   string `json:"title"`
+	ID        string              `json:"id"`
+	Version   string              `json:"version"` // branch/version slug
+	Title     string              `json:"title"`
+	UpdatedAt string              `json:"updated_at,omitzero"`
+	CreatedAt time.Time           `json:"created_at"`
+	Filename  string              `json:"filename"`
+	LegacyId  string              `json:"legacy_id"`
+	Source    APIDefinitionSource `json:"source"`
+	StatusUrl string              `json:"status_url"`
+	Type      string              `json:"type"`
+	Upload    APIDefinitionUpload `json:"upload"`
+	Uri       string              `json:"uri"`
+}
+
+type APIDefinitionSource struct {
+	Current  string `json:"current"`
+	Original string `json:"original"`
+	SyncUrl  string `json:"sync_url"`
+}
+type APIDefinitionUpload struct {
+	Status   string `json:"status"`
+	Reason   string `json:"reason"`
+	Warnings string `json:"warnings"`
 }
 
 // APIDefinitionParams is the request body for creating/updating/validating an API definition.
